561e50c85f5fb5bb3ce657bdcafd3071b66effae
kkwol <kkwolny@vp.pl>
2020-02-23 22:19:15 +0100

    Initial commit

---
+++ src/main/java/com/doeveryday/doeveryday/DoEverydayApplication.java
@@ -0,0 +1,13 @@
+package com.doeveryday.doeveryday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoEverydayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoEverydayApplication.class, args);
+    }
+
+}
