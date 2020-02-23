9dbc4d365c186662717979fb0fff7e410c7117e3
kkwol <kkwolny@vp.pl>
2020-02-23 23:37:11 +0100

    Add todo module. closes #9

---
+++ do-everyday-todo/src/main/java/com/doeveryday/doeverydaytodo/DoEverydayTodoApplication.java
@@ -0,0 +1,13 @@
+package com.doeveryday.doeverydaytodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoEverydayTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoEverydayTodoApplication.class, args);
+    }
+
+}
