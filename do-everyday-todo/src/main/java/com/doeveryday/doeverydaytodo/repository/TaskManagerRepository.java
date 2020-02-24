3e68ccdeb35a011849a06b6f560dd367ad457a88
kkwol <kkwolny@vp.pl>
2020-02-24 13:19:41 +0100

    Adding repository annotation(task #18)

--- do-everyday-todo/src/main/java/com/doeveryday/doeverydaytodo/repository/TaskManagerRepository.java
+++ do-everyday-todo/src/main/java/com/doeveryday/doeverydaytodo/repository/TaskManagerRepository.java
@@ -2,9 +2,11 @@ package com.doeveryday.doeverydaytodo.repository;
 
 import com.doeveryday.doeverydaytodo.models.TaskManager;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TaskManagerRepository extends CrudRepository<TaskManager, Long> {
 
     List<TaskManager> findByDoneIsTrue();
--- do-everyday-todo/src/main/java/com/doeveryday/doeverydaytodo/repository/TaskMemberRepository.java
+++ do-everyday-todo/src/main/java/com/doeveryday/doeverydaytodo/repository/TaskMemberRepository.java
@@ -2,6 +2,8 @@ package com.doeveryday.doeverydaytodo.repository;
 
 import com.doeveryday.doeverydaytodo.models.TaskMember;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskMemberRepository extends CrudRepository<TaskMember, Long> {
 }
