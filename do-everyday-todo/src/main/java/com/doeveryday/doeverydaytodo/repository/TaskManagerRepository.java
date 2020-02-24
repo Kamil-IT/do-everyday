ab99f3c56130dc65a55acfce4b986a7d8486af53
kkwol <kkwolny@vp.pl>
2020-02-24 17:58:32 +0100

    Clean up

--- do-everyday-todo/src/main/java/com/doeveryday/doeverydaytodo/repository/TaskManagerRepository.java
+++ do-everyday-todo/src/main/java/com/doeveryday/doeverydaytodo/repository/TaskManagerRepository.java
@@ -4,12 +4,6 @@ import com.doeveryday.doeverydaytodo.models.TaskManager;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface TaskManagerRepository extends CrudRepository<TaskManager, Long> {
-
-    List<TaskManager> findByDoneIsTrue();
-
-    List<TaskManager> findByDoneIsFalse();
 }
