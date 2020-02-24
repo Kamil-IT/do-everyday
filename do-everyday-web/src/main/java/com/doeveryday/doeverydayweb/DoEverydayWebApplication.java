2b970c30a186d8591da7f4dcb62783c56c4a97c5
kkwol <kkwolny@vp.pl>
2020-02-24 03:07:27 +0100

    Add web module. closes #10

---
+++ do-everyday-web/src/main/java/com/doeveryday/doeverydayweb/DoEverydayWebApplication.java
@@ -0,0 +1,25 @@
+package com.doeveryday.doeverydayweb;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Slf4j
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.doeveryday.*"})
+@EntityScan(basePackages = {"com.doeveryday.*"})
+@EnableJpaRepositories(basePackages = {"com.doeveryday.*"})
+public class DoEverydayWebApplication {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoEverydayWebApplication.class, args);
+        System.out.println("alllll");
+
+    }
+
+}
