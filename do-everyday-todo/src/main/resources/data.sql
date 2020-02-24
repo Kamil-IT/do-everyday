1fbe97c1433ff737281cce0ae8fde769893b3706
kkwol <kkwolny@vp.pl>
2020-02-24 15:01:20 +0100

    Fixed data.sql

--- do-everyday-todo/src/main/resources/data.sql
+++ do-everyday-todo/src/main/resources/data.sql
@@ -2,9 +2,9 @@ INSERT INTO board (name, color_name) values ('Home', 'blue');
 INSERT INTO board (name, color_name) values ('Studies', 'red');
 
 
-INSERT INTO task (name, description, bord_id) values ('Clean room', 'mum will be angry when you dont do this', 1);
-INSERT INTO task (name, description, bord_id) values ('Throw bottle to trash been', 'mum will be angry when you dont do this', 1);
-INSERT INTO task (name, description, bord_id) values ('Make Experiment', 'Experiment for physics', 2);
+INSERT INTO task (name, description, board_id) values ('Clean room', 'mum will be angry when you dont do this', 1);
+INSERT INTO task (name, description, board_id) values ('Throw bottle to trash been', 'mum will be angry when you dont do this', 1);
+INSERT INTO task (name, description, board_id) values ('Make Experiment', 'Experiment for physics', 2);
 
 INSERT INTO task_manager (task_id, is_done) values (1, FALSE);
 
