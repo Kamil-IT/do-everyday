bf8cff7884e91b6beb873fd85a2777ba8ce6a68a
kkwol <kkwolny@vp.pl>
2020-02-27 10:47:01 +0100

    Adding NullPointerException

---
+++ do-everyday-todo/src/main/java/com/doeveryday/doeverydaytodo/exceptions/NullPointerException.java
@@ -0,0 +1,15 @@
+package com.doeveryday.doeverydaytodo.exceptions;
+
+public class NullPointerException extends RuntimeException {
+    public NullPointerException(){
+        super();
+    }
+
+    public NullPointerException(String message){
+        super(message);
+    }
+
+    public NullPointerException(String message, Throwable cause){
+        super(message, cause);
+    }
+}
