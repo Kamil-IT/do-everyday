8f09bd411d49346a0387c7c3233b5c5f091980f8
kkwol <kkwolny@vp.pl>
2020-02-28 22:44:50 +0100

    Adding forecast controller and layout. closes #44

--- do-everyday-web/src/main/java/com/doeveryday/doeverydayweb/controller/weather/ForecastController.java
+++ do-everyday-web/src/main/java/com/doeveryday/doeverydayweb/controller/weather/ForecastController.java
@@ -1,4 +1,22 @@
 package com.doeveryday.doeverydayweb.controller.weather;
 
+import com.doeveryday.doeverydayweather.service.ForecastService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@AllArgsConstructor
+@Controller
 public class ForecastController {
+
+    private final ForecastService forecastService;
+
+    @GetMapping("weather/forecast")
+    public String getCurrentWeather(Model model){
+        model.addAttribute("hourly", forecastService.getHourlyForecast());
+        model.addAttribute("daily", forecastService.getDailyForecast());
+
+        return "weather/forecast";
+    }
 }
