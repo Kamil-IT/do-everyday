656b064344e717d4e8840474a28dea760b4a6c3f
kkwol <kkwolny@vp.pl>
2020-02-28 23:04:46 +0100

    Adding Shopping list to data

--- do-everyday-todo/src/main/resources/data.sql
+++ do-everyday-todo/src/main/resources/data.sql
@@ -1,10 +1,14 @@
 INSERT INTO board (name, color_name) values ('Home', 'blue');
 INSERT INTO board (name, color_name) values ('Studies', 'red');
+INSERT INTO board (name, color_name) values ('Shopping list', 'blue');
 
 
 INSERT INTO task (name, description, board_id) values ('Clean room', 'mum will be angry when you dont do this', 1);
 INSERT INTO task (name, description, board_id) values ('Throw bottle to trash been', 'mum will be angry when you dont do this', 1);
 INSERT INTO task (name, description, board_id) values ('Make Experiment', 'Experiment for physics', 2);
+INSERT INTO task (name, description, board_id) values ('Milk', '0,5L', 3);
+INSERT INTO task (name, description, board_id) values ('Bread', '2', 3);
+INSERT INTO task (name, description, board_id) values ('Chicken', '1kg', 3);
 
 INSERT INTO task_manager (task_id, is_done) values (1, FALSE);
 
