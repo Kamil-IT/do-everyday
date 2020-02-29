b7f74fea0cb678c095b7fd7ab58b91aea01c3eac
kkwol <kkwolny@vp.pl>
2020-02-29 23:31:12 +0100

    Imported api class for Spotify and added Bean SpotifyApi. closes #55

---
+++ do-everyday-spotify/src/main/java/com/doeveryday/doeverydayspotify/config/SpotifyApiConfiguration.java
@@ -0,0 +1,49 @@
+package com.doeveryday.doeverydayspotify.config;
+
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
+import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Slf4j
+@Configuration
+public class SpotifyApiConfiguration {
+
+    @Value("${spotify.api.client.id}")
+    private String CLIENT_ID;
+
+    @Value("${spotify.api.client.secret}")
+    private String CLIENT_SECRET;
+
+    @Bean
+    public SpotifyApi spotifyApi() {
+
+//        Setting keys
+        SpotifyApi spotifyApi = new SpotifyApi.Builder()
+                .setClientId(CLIENT_ID)
+                .setClientSecret(CLIENT_SECRET)
+                .build();
+
+//        Create credentials
+        ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials().build();
+
+        try {
+            final ClientCredentials clientCredentials;
+            clientCredentials = clientCredentialsRequest.execute();
+
+            // Set access token for further "spotifyApi" object usage
+            spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+            return spotifyApi;
+        } catch (IOException | SpotifyWebApiException e) {
+            log.error(e.getMessage());
+            e.printStackTrace();
+            return spotifyApi;
+        }
+    }
+}
