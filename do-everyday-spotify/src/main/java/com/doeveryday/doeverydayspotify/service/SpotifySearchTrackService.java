dc146654f3ac6082d66799436531daa287bc0a18
kkwolny <kkwolny@vp.pl>
2020-03-02 16:22:33 +0100

    Made service for spotify searching tracks by name and added possibility to limit response. closes #61

---
+++ do-everyday-spotify/src/main/java/com/doeveryday/doeverydayspotify/service/SpotifySearchTrackService.java
@@ -0,0 +1,43 @@
+package com.doeveryday.doeverydayspotify.service;
+
+import com.doeveryday.doeverydayspotify.exception.SpotifyApiException;
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.specification.Track;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class SpotifySearchTrackService {
+
+    private final SpotifyApi spotifyApi;
+
+    public List<Track> getTracksByName(String name){
+        List<Track> tracks = new ArrayList<>();
+        try {
+            Track[] items = spotifyApi.searchTracks(name).build().execute().getItems();
+            tracks.addAll(Arrays.asList(items));
+        } catch (IOException | SpotifyWebApiException e) {
+            throw new SpotifyApiException("HTTP status code 4** or 5** has been returned in a request");
+        }
+        return tracks;
+    }
+
+    public List<Track> getTracksByName(String name, Integer limit){
+        List<Track> tracks = new ArrayList<>();
+        try {
+            Track[] items = spotifyApi.searchTracks(name).limit(limit).build().execute().getItems();
+            tracks.addAll(Arrays.asList(items));
+        } catch (IOException | SpotifyWebApiException e) {
+            throw new SpotifyApiException("HTTP status code 4** or 5** has been returned in a request");
+        }
+        return tracks;
+    }
+
+}
