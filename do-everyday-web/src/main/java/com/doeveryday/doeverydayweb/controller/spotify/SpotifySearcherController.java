1814523febbade515107497b57c8d87432128b50
kkwol <kkwolny@vp.pl>
2020-03-04 22:06:49 +0100

    Added photo to results of spotify searcher. closes #70

--- do-everyday-web/src/main/java/com/doeveryday/doeverydayweb/controller/spotify/SpotifySearcherController.java
+++ do-everyday-web/src/main/java/com/doeveryday/doeverydayweb/controller/spotify/SpotifySearcherController.java
@@ -2,37 +2,39 @@ package com.doeveryday.doeverydayweb.controller.spotify;
 
 import com.doeveryday.doeverydayspotify.model.SpotifyAllTypes;
 import com.doeveryday.doeverydayspotify.service.SpotifySearchService;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@AllArgsConstructor
 @Slf4j
 @Controller
 public class SpotifySearcherController {
 
-    SpotifySearchService spotifySearchService;
+    private SpotifySearchService spotifySearchService;
+    private SpotifyAllTypes searchResult;
+
+    public SpotifySearcherController(SpotifySearchService spotifySearchService) {
+        this.spotifySearchService = spotifySearchService;
+    }
 
     @GetMapping("spotify/search")
     public String initAllTypesSearch(Model model,
                                      @RequestParam(name = "name", required = false) String name,
                                      @RequestParam(name = "limit", required = false) Integer limit) {
-        SpotifyAllTypes searchResult;
+
         if (name != null && limit != null) {
-            if (name.length() == 0){
+            if (name.length() == 0) {
                 return "spotify/search/searchform";
             }
             searchResult = spotifySearchService.spotifyAllTypesByName(name, limit);
-        } else if (name != null){
-            if (name.length() == 0){
+        } else if (name != null) {
+            if (name.length() == 0) {
                 return "spotify/search/searchform";
             }
             searchResult = spotifySearchService.spotifyAllTypesByName(name);
-        }
-        else {
+        } else {
             return "spotify/search/searchform";
         }
 
