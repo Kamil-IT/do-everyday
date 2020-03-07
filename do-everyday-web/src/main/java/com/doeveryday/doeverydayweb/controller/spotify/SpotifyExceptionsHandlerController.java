25996fcb5cdb48bdfc454112b2631735c69219b0
kkwol <kkwolny@vp.pl>
2020-03-07 14:59:00 +0100

    Renamed SpotifyExceptionsHandlerController

--- do-everyday-web/src/main/java/com/doeveryday/doeverydayweb/controller/spotify/SpotifyExceptionsHandlerController.java
+++ do-everyday-web/src/main/java/com/doeveryday/doeverydayweb/controller/spotify/SpotifyExceptionsHandlerController.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Slf4j
 @AllArgsConstructor
 @ControllerAdvice
-public class ControllerSpotifyExceptionsHandler {
+public class SpotifyExceptionsHandlerController {
 
     private ErrorPagesController errorPagesController;
 
