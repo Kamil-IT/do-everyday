4531a0840b96c25d87b591579d4e1bbeb53f271b
kkwol <kkwolny@vp.pl>
2020-03-07 14:02:52 +0100

    Added page to handling errors. closes #80

---
+++ do-everyday-web/src/main/java/com/doeveryday/doeverydayweb/controller/ErrorPagesController.java
@@ -0,0 +1,69 @@
+package com.doeveryday.doeverydayweb.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@Controller
+public class ErrorPagesController {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @RequestMapping("/error/notfoundpage")
+    public ModelAndView handleErrorPageNotFound(Exception e) {
+        log.error("Page not found. " + e.getMessage());
+        e.printStackTrace();
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("error404");
+        modelAndView.addObject("exception", e.getMessage());
+        modelAndView.setStatus(HttpStatus.NOT_FOUND);
+
+        return modelAndView;
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @RequestMapping("/error/internalservererror")
+    public ModelAndView handleErrorInternalServerError(Exception e) {
+        log.error("Server make some error. " + e.getMessage());
+        e.printStackTrace();
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("error404");
+        modelAndView.addObject("exception", e.getMessage());
+        modelAndView.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        return modelAndView;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @RequestMapping("/error/badrequest")
+    public ModelAndView handleErrorBadRequest(Exception e) {
+        log.error("Bad request error. " + e.getMessage());
+        e.printStackTrace();
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("error404");
+        modelAndView.addObject("exception", e.getMessage());
+        modelAndView.setStatus(HttpStatus.BAD_REQUEST);
+
+        return modelAndView;
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @RequestMapping("/error/notfound")
+    public ModelAndView handleErrorNotFound(Exception e) {
+        log.error("Not found some stuff. " + e.getMessage());
+        e.printStackTrace();
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("error404");
+        modelAndView.addObject("exception", e.getMessage());
+        modelAndView.setStatus(HttpStatus.NOT_FOUND);
+
+        return modelAndView;
+    }
+}
--- do-everyday-web/src/main/java/com/doeveryday/doeverydayweb/controller/HandlerWhitelabelErrorPage.java
+++ do-everyday-web/src/main/java/com/doeveryday/doeverydayweb/controller/HandlerWhitelabelErrorPage.java
@@ -2,29 +2,14 @@ package com.doeveryday.doeverydayweb.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
 public class HandlerWhitelabelErrorPage implements ErrorController {
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @RequestMapping("/error")
-    public ModelAndView handleError() {
-        log.error("Page not found...");
-
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("error404");
-
-        return modelAndView;
-    }
-
     @Override
     public String getErrorPath() {
-        return "/error";
+        return "/error/notfoundpage";
     }
 }
