561ff0f365b6d7b0f95657d84af27c387faeaebb
kkwol <kkwolny@vp.pl>
2020-03-07 14:13:48 +0100

    Handled errror for todo

---
+++ do-everyday-web/src/main/java/com/doeveryday/doeverydayweb/controller/todo/TodoExceptionsHandlerController.java
@@ -0,0 +1,28 @@
+package com.doeveryday.doeverydayweb.controller.todo;
+
+import com.doeveryday.doeverydaytodo.exceptions.NotFoundException;
+import com.doeveryday.doeverydaytodo.exceptions.NullPointerException;
+import com.doeveryday.doeverydayweb.controller.ErrorPagesController;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@AllArgsConstructor
+@ControllerAdvice
+public class TodoExceptionsHandlerController {
+
+    private final ErrorPagesController errorPagesController;
+
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(NotFoundException expectation){
+        return errorPagesController.handleErrorNotFound(expectation);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ModelAndView handleNullPointerExceptions(NullPointerException expectation){
+        return errorPagesController.handleErrorBadRequest(expectation);
+    }
+}
