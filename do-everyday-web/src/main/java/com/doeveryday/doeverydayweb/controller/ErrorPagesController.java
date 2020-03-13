3ad3462b89c87ee1cc5623adeab609252d77c12c
kkwol <kkwolny@vp.pl>
2020-03-13 14:01:59 +0100

    Add pages which show all errors. closes #90

--- do-everyday-web/src/main/java/com/doeveryday/doeverydayweb/controller/ErrorPagesController.java
+++ do-everyday-web/src/main/java/com/doeveryday/doeverydayweb/controller/ErrorPagesController.java
@@ -11,6 +11,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ErrorPagesController {
 
+    private void printStackError(Exception e){
+        if (e.getMessage() != null){
+            log.error(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @RequestMapping("/error")
     public ModelAndView handleErrorPageNotFound() {
@@ -19,6 +26,7 @@ public class ErrorPagesController {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("error404");
         modelAndView.addObject("exception", "Page not found");
+        modelAndView.addObject("httpstatus", "404 Not Found");
         modelAndView.setStatus(HttpStatus.NOT_FOUND);
 
         return modelAndView;
@@ -27,12 +35,15 @@ public class ErrorPagesController {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @RequestMapping("/error/internalservererror")
     public ModelAndView handleErrorInternalServerError(Exception e) {
-        log.error("Server make some error. " + e.getMessage());
-        e.printStackTrace();
+        log.error("Server make some error. ");
+        printStackError(e);
 
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("error404");
-        modelAndView.addObject("exception", e.getMessage());
+        if (e != null){
+            modelAndView.addObject("exception", e.getMessage());
+        }
+        modelAndView.addObject("httpstatus", "500 Internal Server Error");
         modelAndView.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
 
         return modelAndView;
@@ -41,12 +52,15 @@ public class ErrorPagesController {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @RequestMapping("/error/badrequest")
     public ModelAndView handleErrorBadRequest(Exception e) {
-        log.error("Bad request error. " + e.getMessage());
-        e.printStackTrace();
+        log.error("Bad request error.");
+        printStackError(e);
 
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("error404");
-        modelAndView.addObject("exception", e.getMessage());
+        if (e != null){
+            modelAndView.addObject("exception", e.getMessage());
+        }
+        modelAndView.addObject("httpstatus", "400 Bad Request");
         modelAndView.setStatus(HttpStatus.BAD_REQUEST);
 
         return modelAndView;
@@ -55,12 +69,15 @@ public class ErrorPagesController {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @RequestMapping("/error/notfound")
     public ModelAndView handleErrorNotFound(Exception e) {
-        log.error("Not found some stuff. " + e.getMessage());
-        e.printStackTrace();
+        log.error("Not found some stuff.");
+        printStackError(e);
 
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("error404");
-        modelAndView.addObject("exception", e.getMessage());
+        if (e != null){
+            modelAndView.addObject("exception", e.getMessage());
+        }
+        modelAndView.addObject("httpstatus", "404 Not Found");
         modelAndView.setStatus(HttpStatus.NOT_FOUND);
 
         return modelAndView;
