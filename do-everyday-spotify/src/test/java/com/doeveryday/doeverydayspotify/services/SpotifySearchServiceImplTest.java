3bc559e6d94d712d272e204701ce644ca2f02257
kkwolny <kkwolny@vp.pl>
2020-03-26 13:44:35 +0100

    Added empty test file

---
+++ do-everyday-spotify/src/test/java/com/doeveryday/doeverydayspotify/services/SpotifySearchServiceImplTest.java
@@ -0,0 +1,21 @@
+package com.doeveryday.doeverydayspotify.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpotifySearchServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void spotifyAllTypesByName() {
+    }
+
+    @Test
+    void testSpotifyAllTypesByName() {
+    }
+}
