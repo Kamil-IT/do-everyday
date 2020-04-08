790355ffe0861d0d852e9d7a932dd3b0c0e3d19a
kkwol <kkwolny@vp.pl>
2020-04-08 23:08:50 +0200

    Added module with is responsible for security. closes #109

---
+++ do-everyday-security/src/test/java/com/doeveryday/doeverydaysecurity/DoEverydaySecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doeveryday.doeverydaysecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoEverydaySecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
