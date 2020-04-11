23e18dcaaaa66bd00529e00b96e58936b20ef3e7
kkwol <kkwolny@vp.pl>
2020-04-11 14:31:08 +0200

    Clean up

--- do-everyday-security/src/main/java/com/doeveryday/doeverydaysecurity/config/AppSecurityH2ConsoleConfig.java
+++ do-everyday-security/src/main/java/com/doeveryday/doeverydaysecurity/config/AppSecurityH2ConsoleConfig.java
@@ -1,5 +1,8 @@
 package com.doeveryday.doeverydaysecurity.config;
 
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -9,15 +12,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
+/**
+ * H2 console enabled on .../console
+ */
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
-@Profile("mysql")
-public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+@Profile("default")
+public class AppSecurityH2ConsoleConfig extends WebSecurityConfigurerAdapter {
 
     private final DaoAuthenticationProvider daoAuthenticationProvider;
 
-    public AppSecurityConfig(DaoAuthenticationProvider daoAuthenticationProvider) {
+    public AppSecurityH2ConsoleConfig(DaoAuthenticationProvider daoAuthenticationProvider) {
         this.daoAuthenticationProvider = daoAuthenticationProvider;
     }
 
@@ -34,7 +41,12 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .logoutUrl("/logout")
-                .logoutSuccessUrl("/login?logout=true");
+                .logoutSuccessUrl("/login?logout=true")
+                .and()
+//                Config for h2 console
+                .headers()
+                .frameOptions()
+                .disable();
     }
 
 
@@ -42,5 +54,16 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) {
         auth
                 .authenticationProvider(daoAuthenticationProvider);
+    }
+
+//    Config for h2 console
+    @SuppressWarnings("rawtypes")
+    @Profile("default")
+    @Bean
+    public ServletRegistrationBean h2servletRegistration() {
+        //noinspection unchecked
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
     }
 }
